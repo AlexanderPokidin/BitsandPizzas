b79cdb61dcf407ca19e8d51d3bf64c3e7b983494
AlexanderPokidin <a.pokidin@gmail.com>
2017-08-20 20:27:44 +0300

    Commit selectItem method

--- app/src/main/java/com/example/alex/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/example/alex/bitsandpizzas/MainActivity.java
@@ -5,15 +5,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ShareActionProvider;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 
 public class MainActivity extends Activity {
 
     private ShareActionProvider shareActionProvider;
     private String[] titles;
     private ListView drawerList;
+
+    private class DriwerItemClickListener implements ListView.OnItemClickListener{
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position){
+        Fragment fragment;
+        switch (position){
+            case 1:
+                fragment = new PizzaFragment();
+                break;
+            case 2:
+                fragment = new PastaFragment();
+                break;
+            case 3:
+                fragment = new StoresFragment();
+                break;
+            default:
+                fragment = new TopFragment();
+        }
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment);
+        ft.addToBackStack(null);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +56,7 @@ public class MainActivity extends Activity {
         titles = getResources().getStringArray(R.array.titles);
         drawerList = (ListView)findViewById(R.id.drawer_layout);
         drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, titles));
+        drawerList.setOnItemClickListener(new DriwerItemClickListener());
     }
 
     @Override
